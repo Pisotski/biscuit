42b7189c0fde129cc2226514c562e1657cc5ecf1
Poisk <vpisotski@gmail.com>
2019-06-30 11:49:34 -0700

    commit before material ui table  Please enter the commit message for your changes. Lines starting

--- src/components/subTable/SubTable.js
+++ src/components/subTable/SubTable.js
@@ -26,8 +26,7 @@ export default class SubTable extends Component {
     if (!SubTable) return 'Loading async data...';
 
     return (
-      <div className={styles.SubTable} >
-        <h2>Earnings</h2>
+      <div className={styles.SubTable}>
         <ReactTable
           data={this.props.SubTable}
           columns={columns}
